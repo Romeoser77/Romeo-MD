441ff95717781b822972819a7fbac58effabf398
Romeoser77 <romeomods26@gmail.com>
2025-04-19 15:31:25 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@
  You may not sell this script.
  It is supplied in the hope that it may be useful.
  * @project_name : Free Bot script
- * @author : Malvin King <https://github.com/kingmalvn>
+ * @author : ᴅᴇᴠᴇʟᴏᴘᴇᴅ ʙʏ ʀᴏᴍᴇᴏᴍᴏᴅꜱ ᴀɴᴅ ɢᴏʟᴅᴇɴ ᴅʀᴀɢᴏɴ
  * @description : A Multi-functional whatsapp bot script.
  * @version 3.0.0
  **/
@@ -25,8 +25,8 @@ MODE: process.env.MODE || "private",
 AUTO_VOICE: process.env.AUTO_VOICE || "false",
 AUTO_STICKER: process.env.AUTO_STICKER || "false",
 AUTO_REPLY: process.env.AUTO_REPLY || "false",
-ALIVE_IMG: process.env.ALIVE_IMG || "your alive image url",
-MENU_IMG: process.env.MENU_IMG || "your menu pic url",
+ALIVE_IMG: process.env.ALIVE_IMG || "https://i.imgur.com/CA4S4qX.jpeg",
+MENU_IMG: process.env.MENU_IMG || "https://i.imgur.com/dWHq9jO.jpeg",
 ALIVE_MSG: process.env.ALIVE_MSG || "_Hi 💁🏽 How Can I Assist You. Am alive Now._",
 ANTI_LINK: process.env.ANTI_LINK || "true",
 ANTI_BAD: process.env.ANTI_BAD || "true",
@@ -38,6 +38,6 @@ CURRENT_STATUS: process.env.CURRENT_STATUS || "true",
 AUTO_REACT: process.env.AUTO_REACT || "false",
 HEART_REACT: process.env.HEART_REACT || "false",
 OWNER_REACT: process.env.OWNER_REACT || "false",
-BOT_NAME: process.env.BOT_NAME || "『YOUR BOTNAME 』",
+BOT_NAME: process.env.BOT_NAME || "『ᴢᴇɴᴏ-ᴍᴅ ᴠ1』",
 OMDB_API_KEY: process.env.OMDB_API_KEY || "76cb7f39", // omdbapi.com
 };
